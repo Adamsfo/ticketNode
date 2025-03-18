045bb454bb5a4f99271c53bf9fd227b8e7ddbc9f
Adams Figueroa de Oliveira <adamsfo20@gmail.com>
2025-03-18 16:57:05 -0400

    ingerssos

--- src/routes/eventoIngressoRoute.ts
+++ src/routes/eventoIngressoRoute.ts
@@ -3,7 +3,7 @@ const router = express.Router()
 const { authenticate } = require('../middlewares/authMiddleware')
 const EventoIngressoController = require('../controllers/EventoIngressoController')
 
-router.get('/eventoingresso', authenticate, EventoIngressoController.get)
+router.get('/eventoingresso', EventoIngressoController.get)
 router.post('/eventoingresso', authenticate, EventoIngressoController.add)
 router.put('/eventoingresso/:id', authenticate, EventoIngressoController.edit)
 router.delete('/eventoingresso/:id', authenticate, EventoIngressoController.delete)
--- src/routes/eventoRoutes.ts
+++ src/routes/eventoRoutes.ts
@@ -3,7 +3,7 @@ const router = express.Router()
 const { authenticate } = require('../middlewares/authMiddleware')
 const EventoController = require('../controllers/EventoController')
 
-router.get('/evento', authenticate, EventoController.get)
+router.get('/evento', EventoController.get)
 router.post('/evento', authenticate, EventoController.add)
 router.put('/evento/:id', authenticate, EventoController.edit)
 router.delete('/evento/:id', authenticate, EventoController.delete)
