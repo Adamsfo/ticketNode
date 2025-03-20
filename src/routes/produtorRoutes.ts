17574e3d1147b08786dc3441a32923314f06890f
Adams Figueroa de Oliveira <adamsfo20@gmail.com>
2025-03-20 05:20:10 -0400

    page ingressos finalizada

--- src/routes/produtorRoutes.ts
+++ src/routes/produtorRoutes.ts
@@ -3,7 +3,7 @@ const router = express.Router()
 const { authenticate } = require('../middlewares/authMiddleware')
 const ProdutorController = require('../controllers/ProdutorController')
 
-router.get('/produtor', authenticate, ProdutorController.get)
+router.get('/produtor', ProdutorController.get)
 router.post('/produtor', authenticate, ProdutorController.add)
 router.put('/produtor/:id', authenticate, ProdutorController.edit)
 router.delete('/produtor/:id', authenticate, ProdutorController.delete)
